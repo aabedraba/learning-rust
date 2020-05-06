a0ee88c5db248e03a894bf570cbc11853158331e
Abdallah <aabedraba@gmail.com>
2020-05-06 20:36:01 +0200

    Chapter 2: Completed guessing game!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use std::io::stdin;
-
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number game!");
-    println!("Please input your number: ");
+    println!("--Guessing time!--");
 
-    let mut number = String::new();
+    let mut secretNumer = rand::thread_rng().gen_range(1, 101);
+    
+    println!("Random number generated. Time to guess your own number!");
 
-    stdin().read_line(&mut number)
-        .expect("Failed to read line.");
-
-    println!("You guessed: {}", number);
+    loop {
+        println!("Input new number: ");
+        let mut guessed = String::new();
+        std::io::stdin().read_line(&mut guessed)
+            .expect("Fail reading variable");
+        
+        let guessed: u32 = match guessed.trim().parse() {
+            Ok(num) => num, 
+            Err(_) => continue,
+        };
+        
+        match guessed.cmp(& secretNumer) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("Nailed it!");
+                break;
+            }
+        };
+    } 
 }
